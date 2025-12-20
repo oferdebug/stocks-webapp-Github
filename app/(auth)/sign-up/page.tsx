22c1869634f85a53137347665de9e31475fd8661
oferdebug <ofercohen404@gmail.com>
2025-12-20 08:15:38 +0200

    recovered partial code from Ctrl+Z

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,50 @@
 'use client';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import {Button} from "@/components/ui/button";
+
+
+
+
 function SignUp() {
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors,isSubmitting },
+    } = useForm<SignUpFormData>({
+        defaultValues:{
+        email:'',
+        fullName:'',
+        password:'',
+        country: 'US',
+        investmentGoals:'Growth',
+        riskTolerance:'Medium',
+        preferredIndustry:'Technology'
+        },
+
+        mode:'onBlur'
+    });
+
+    const onSubmit = async (data:SignUpFormData) => {
+        try {
+            console.log(data);
+        } catch (e) {
+        console.error(e);
+        }
+    }
+
     return (
-        <div>SignUp</div>
-    )
+        <div className={'relative overflow-hidden'}>
+            <h1 className="form-title">SignUp & Personalize Your Experience</h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} className={'space-y-6'}>
+            {/*  Inputs  */}
+                <Button type={'submit'} disabled={isSubmitting} className={'green-btn w-full mt-5'}>
+                    {isSubmitting?'Creating Account':'Start Your Investing Journey Today'}
+                </Button>
+            </form>
+        </div>
+    );
 }
 
-export default SignUp
+export default SignUp;
